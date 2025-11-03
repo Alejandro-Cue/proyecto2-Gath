e6e517789f5c8e347443267dc29f8f196a4b5162
Alejandro Cué <alejcue@gmail.com>
2025-11-03 11:54:07 +0100

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/proyecto2-Gath/', 
+  base: '/proyecto2-Gath/',
+  build: {
+    outDir: 'docs'
+  } 
 })
